9c4903600402503c5a8c7d04cead5478f6c7210f
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-01-05 10:03:54 -0500

    progress

--- Scripts/MacPersistenceChecker.sh
+++ Scripts/MacPersistenceChecker.sh
@@ -6,3 +6,11 @@ ls -la /Library/Launch*
 
 echo "Also checking the home directory of the current user:"
 ls -la ~/Library/LaunchAgents
+
+echo "Do any crontabs exist for the current user?"
+crontab -l
+#/private/var/at/tabs should contain ALL crontabs on the system, but requires root access to view (similar to at jobs)
+
+echo "Checking for periodic scripts"
+ls ls /etc/periodic/*
+#Note: this is not commonly used for malware
