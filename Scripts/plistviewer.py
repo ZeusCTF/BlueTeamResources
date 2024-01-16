ec0b70b6c3b0456cdb1f2b490310dca27e77cf2c
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-01-16 10:24:52 -0500

    cleaning up a bit

--- Scripts/plistviewer.py
+++ Scripts/plistviewer.py
@@ -1,6 +1,5 @@
 import argparse
 import plistlib
-from biplist import *
 
 # Grab file path
 parser = argparse.ArgumentParser()
@@ -21,44 +20,11 @@ plutil -convert xml1 <file>
 With both of the forms above being vaild, binary is just more efficient on disk.
 """
 
-if args.file == True:
-    pass
-# Specify the path to your plist file
-binary_plist_path = '/Users/barryallen/Test/org.virtualbox.app.VirtualBox.plist'
 xml_plist_path = '/Users/barryallen/Test/org.virtualbox.app.VirtualBox.plist1'
-
-try:
-    plist = readPlist(binary_plist_path)
-    print("Binary attempt:")
-    print(plist)
-    print()
-except(InvalidPlistException, NotBinaryPlistException) as e:
-    print("Not a plist:", e)
-
-try:
-    plist = readPlist(xml_plist_path)
-    print("XML attempt:")
-    print(plist)
-    print()
-except(InvalidPlistException, NotBinaryPlistException) as e:
-    print("Not a plist:", e)
-
-
-# Open the plist file
-with open(binary_plist_path, 'rb') as fp:
-    # Load the plist data
-    plist_data = plistlib.load(fp)
-
-# Now you can work with the plist data as a Python dictionary
-print("Binary plistlib:")
-print(plist_data)
-
 
 # Open the plist file
 with open(xml_plist_path, 'rb') as fp:
     # Load the plist data
-    plist_data = plistlib.load(fp)
-
-# Now you can work with the plist data as a Python dictionary
-print("XML plistlib:")
-print(plist_data)
+    test = plistlib.load(fp)
+    for k,v in test.items():
+        print(k,v)
