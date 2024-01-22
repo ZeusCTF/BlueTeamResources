eab07f9868a9149f6175028c75679a69add94484
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-01-22 15:33:17 -0500

    progress

--- Scripts/plistPolisher.py
+++ Scripts/plistPolisher.py
@@ -1,11 +1,3 @@
-import argparse
-import plistlib
-
-# Grab file path
-parser = argparse.ArgumentParser()
-parser.add_argument('file', type=str, help='Please provide the full path to the plist file')
-args = parser.parse_args()
-
 """
 Three plist file types exist; binary, xml, and json (though the first two seem to be the most common):
 
@@ -19,12 +11,14 @@ You can convert the plist files to xml with something like the below(which is wh
 plutil -convert xml1 <file>
 With both of the forms above being vaild, binary is just more efficient on disk.
 """
+import argparse
+import plistlib
 
 xml_plist_path = '/Users/barryallen/Test/org.virtualbox.app.VirtualBox.plist1'
 
 def view(path):
     # Open the plist file
-    with open(xml_plist_path, 'rb') as fp:
+    with open(path, 'rb') as fp:
         # Load the plist data, and print it in a more readable format
         data = plistlib.load(fp)
         for k,v in data.items():
@@ -33,11 +27,39 @@ def view(path):
 
 def edit(path):
     # Open the plist file
-    with open(xml_plist_path, 'rb') as fp:
+    with open(path, 'rb') as fp:
         data = plistlib.load(fp)
+        key = input('Enter the new key, or a key to be updated: ')
+        value = input('Enter the associated value: ')
         # Make changes to the plist data
-        data['key'] = 'new_value'
+        data[key] = value
         with open(xml_plist_path, 'wb') as fp:
             plistlib.dump(data, fp)
             fp.close()
-edit('hello world')
+
+def search(path):
+    with open(path, 'rb') as fp:
+        data = plistlib.load(fp)
+        key = input('Enter the key name you are looking for: ')
+        print()
+        print('Below is the associated value:')
+        print(data[key])
+
+
+def main():
+    # Grab file path
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', type=str, help='Please provide the full path to the plist file')
+    args = parser.parse_args()
+    path = args.file
+
+    print('What would you like to do:')
+    print('Enter 1 to view the entire plist file.')
+    print('Enter 2 to add information or update the plist file.')
+    print('Enter 3 to search for a specific key in the plist file.')
+    print('Enter q to quit.')
+    while True:
+        decision = input('')
+        if decision == 1:
+            view(path)
+main()
