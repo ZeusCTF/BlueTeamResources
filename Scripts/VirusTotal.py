ad8196f61c958fcc381f2fb59783ec7f2e95e2dd
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2024-02-06 10:23:21 -0500

    progress

--- Scripts/VirusTotal.py
+++ Scripts/VirusTotal.py
@@ -1,1 +1,46 @@
-# The idea here is to create a script that utilizes the VT api to determine if an IP address/file hash is suspect
+import vt
+#https://docs.virustotal.com/reference/scan-url
+import requests
+
+url = "https://www.virustotal.com/api/v3/urls"
+
+payload = { "url": "string" }
+headers = {
+    "accept": "application/json",
+    "content-type": "application/x-www-form-urlencoded",
+    "x-apikey": "API KEY"
+}
+
+response = requests.post(url, data=payload, headers=headers)
+
+print(response.text)
+
+
+"""
+#main decision tree
+def main():
+
+    print('What would you like to do:')
+    print('Enter 1 to scan a URL')
+    print('Enter 2 to lookup an IP address')
+    print('Enter 3 to search a file hash.')
+    print('Enter q to quit.')
+
+    decision = input('')
+
+    while decision != 'q':
+        if decision == '1':
+            view(path)
+        elif decision == '2':
+            edit(path)
+        elif decision == '3':
+            search(path)
+
+        print('Enter 1 to view the entire plist file.')
+        print('Enter 2 to add information or update the plist file.')
+        print('Enter 3 to search for a specific key in the plist file.')
+        print('Enter q to quit.')
+        decision = input('What would you like to do next? (Enter q to quit) ')
+
+main()
+"""
